fa8591bf39b85937b76ab39e6101f8d664d930a6
valuvilla <valuvp@hotmail.com>
2023-02-12 17:00:36 +0100

    database

--- menu.py
+++ menu.py
@@ -1,3 +1,14 @@
 import os
+import database as db
 
-print("========","\n BIENVENIDO AL GESTOR, ========")
+def Iniciar():
+    os.system('cls')
+    print("", "="*25,"\n BIENVENIDO AL GESTOR \n", "="*25, "\n[1] Listar los clientes \n[2] Buscar un cliente \n[3] Agregar un cliente \n[4] Modificar un cliente \n[5] Eliminar un cliente \n[6] Salir", "\n", "="*25)
+
+    opcion= input("Ingrese una opción: ")
+    os.system('cls')
+
+    if opcion == "1":
+        print("Listar los clientes")
+        for cliente in Clientes.lista:
+            print(cliente)
