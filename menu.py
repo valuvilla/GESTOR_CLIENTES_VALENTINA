40a0dad87f409ab8342211fbe400f6a6d86f47c9
valuvilla <valuvp@hotmail.com>
2023-02-14 18:47:20 +0100

    guardar

--- menu.py
+++ menu.py
@@ -1,13 +1,25 @@
 import os
 import database as db
 import helpers
+from colorama import *
+init(autoreset=True)
 
 def Iniciar():
     while True:
         helpers.limpiar_pantalla()
-        print("", "="*25,"\n BIENVENIDO AL GESTOR \n", "="*25, "\n[1] Listar los clientes \n[2] Buscar un cliente \n[3] Agregar un cliente \n[4] Modificar un cliente \n[5] Eliminar un cliente \n[6] Salir", "\n", "="*25)
+        
+        print("========================")
+        print("  Bienvenido al Gestor  ")
+        print("========================")
+        print("[1] Listar los clientes ")
+        print("[2] Buscar un cliente   ")
+        print("[3] Añadir un cliente   ")
+        print("[4] Modificar un cliente")
+        print("[5] Borrar un cliente   ")
+        print("[6] Cerrar el Gestor    ") # CERRAR EL MANAGER
+        print("========================")
 
-        opcion= input("Ingrese una opción: ")
+        opcion = input("> ")
         helpers.limpiar_pantalla()
 
         if opcion == '1':
@@ -18,7 +30,7 @@ def Iniciar():
         elif opcion == '2':
             print("Buscando un cliente...\n")
             dni = helpers.leer_texto(3, 3, "DNI (2 int y 1 char)").upper()
-            cliente = db.Clientes.buscar_cliente(dni)
+            cliente = db.Clientes.buscar(dni)
             print(cliente) if cliente else print("Cliente no encontrado.")
 
         elif opcion == '3':
@@ -32,19 +44,19 @@ def Iniciar():
 
             nombre = helpers.leer_texto(2, 30, "Nombre (de 2 a 30 chars)").capitalize()
             apellido = helpers.leer_texto(2, 30, "Apellido (de 2 a 30 chars)").capitalize()
-            db.Clientes.crear(dni, nombre, apellido)
+            db.Clientes.agregar_cliente(dni, nombre, apellido)
             print("Cliente añadido correctamente.")
 
         elif opcion == '4':
             print("Modificando un cliente...\n")
             dni = helpers.leer_texto(3, 3, "DNI (2 int y 1 char)").upper()
-            cliente = db.Clientes.buscar_cliente(dni)
+            cliente = db.Clientes.buscar(dni)
             if cliente:
                 nombre = helpers.leer_texto(
                     2, 30, f"Nombre (de 2 a 30 chars) [{cliente.nombre}]").capitalize()
                 apellido = helpers.leer_texto(
                     2, 30, f"Apellido (de 2 a 30 chars) [{cliente.apellido}]").capitalize()
-                db.Clientes.modificar_cliente(cliente.dni, nombre, apellido)
+                db.Clientes.modificar(cliente.dni, nombre, apellido)
                 print("Cliente modificado correctamente.")
             else:
                 print("Cliente no encontrado.")
@@ -61,4 +73,4 @@ def Iniciar():
 
         input("\nPresiona ENTER para continuar...")
 
-print(Iniciar())
+Iniciar()
