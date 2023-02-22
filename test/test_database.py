521a24f7903e820ae1a271dc84042f56b6693726
valuvilla <valuvp@hotmail.com>
2023-02-22 18:22:25 +0100

    me canse de ,los test

--- test/test_database.py
+++ test/test_database.py
@@ -1,3 +1,4 @@
+
 import csv
 import copy
 import config
@@ -8,25 +9,26 @@ import database as db
 
 class TestDatabase(unittest.TestCase):
 
-    def setUp(self):
-        #Antes de cada test
-        db.Clientes.lista= [
-            db.Cliente('73Y','Valentina','Gonzalez'),
-            db.Cliente('85K','Juan','Perez'),
+    def setUp(self): #Darle valores a la lista de clientes para hacer las pruebas
+        # Se ejecuta antes de cada test
+        db.Clientes.lista = [
+            db.Cliente('15J', 'Marta', 'Pérez'),
+            db.Cliente('48H', 'Manolo', 'López'),
             db.Cliente('28Z', 'Ana', 'García')
         ]
 
     def test_buscar_cliente(self):
-        cliente_existente = db.Clientes.buscar_cliente('73Y')
+        cliente_existente = db.Clientes.buscar_cliente('15J')
         cliente_inexistente = db.Clientes.buscar_cliente('99X')
         self.assertIsNotNone(cliente_existente)
         self.assertIsNone(cliente_inexistente)
+        # assercion, excepcion predeterminada
 
-    def test_agregar_cliente(self):
-        nuevo_cliente = db.Clientes.agregar_cliente('39X', 'María del Carmen', 'Costa')
+    def test_crear_cliente(self):
+        nuevo_cliente = db.Clientes.agregar_cliente('39X', 'Héctor', 'Costa')
         self.assertEqual(len(db.Clientes.lista), 4)
         self.assertEqual(nuevo_cliente.dni, '39X')
-        self.assertEqual(nuevo_cliente.nombre, 'María del Carmen')
+        self.assertEqual(nuevo_cliente.nombre, 'Héctor')
         self.assertEqual(nuevo_cliente.apellido, 'Costa')
 
     def test_modificar_cliente(self):
@@ -35,6 +37,31 @@ class TestDatabase(unittest.TestCase):
         self.assertEqual(cliente_a_modificar.nombre, 'Ana')
         self.assertEqual(cliente_modificado.nombre, 'Mariana')
 
+    def test_borrar_cliente(self):
+        cliente_borrado = db.Clientes.eliminar_cliente('48H')
+        cliente_rebuscado = db.Clientes.buscar_cliente('48H')
+        self.assertEqual(cliente_borrado.dni, '48H')
+        self.assertIsNone(cliente_rebuscado)
+
+    def test_dni_valido(self):
+        self.assertTrue(helpers.dni_valido('00A', db.Clientes.lista))
+        self.assertFalse(helpers.dni_valido('232323S', db.Clientes.lista))
+        self.assertFalse(helpers.dni_valido('F35', db.Clientes.lista))
+        self.assertFalse(helpers.dni_valido('48H', db.Clientes.lista))
+
+    def test_escritura_csv(self):
+        db.Clientes.eliminar_cliente('48H')
+        db.Clientes.eliminar_cliente('15J')
+        db.Clientes.modificar_cliente('28Z', 'Mariana', 'García')
+
+        dni, nombre, apellido = None, None, None
+        with open(config.DATABASE_PATH, newline='\n') as fichero:
+            reader = csv.reader(fichero, delimiter=';')
+            dni, nombre, apellido = next(reader)
+
+        self.assertEqual(dni, '28Z')
+        self.assertEqual(nombre, 'Mariana')
+        self.assertEqual(apellido, 'García')
 
 
 
