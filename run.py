7288cb9ec3e79e9b1f74b0f0a87dc54a6b2b3610
valuvilla <valuvp@hotmail.com>
2023-02-22 17:15:58 +0100

    cambios finales

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import menu
 
 if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "-t":
-        menu.iniciar()
+        menu.Iniciar()
     else:
-        app = ui.MainWindow()
+        app = ui.MainWindows()
         app.mainloop()
